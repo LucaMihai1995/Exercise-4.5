a6f0349d99fb6ee016ef9c9516838d34a915fab5
Luca Mihai <lucam8414@gmail.com>
2019-03-15 13:35:54 +0200

    Luca Mihai

---
+++ Desktop/Java Project/Exercises 3.1/src/edu/tekwill/lesson/three/computer/Computer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.tekwill.lesson.three.computer;
+
+import edu.tekwill.lesson.three.computer.components.HardDrive;
+import edu.tekwill.lesson.three.computer.components.Motherboard;
+import edu.tekwill.lesson.three.computer.components.Processor;
+import edu.tekwill.lesson.three.computer.peripherals.*;
+
+/**
+ *
+ * @author mIHAI
+ */
+public class Computer 
+{
+    Processor processor;
+    Motherboard motherboard;
+    HardDrive harddrive;
+    Keyboard keyboard;
+    Monitor monitor;
+    Mouse mouse;
+}
